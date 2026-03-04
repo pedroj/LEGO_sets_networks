eb0c5ae81420af2b9b8be412b1a1de9b87b0f18e
pedroj <pedroj@me.com>
2026-03-04 18:46:09 +0100

    Add new R scripts and update workflow files

---
+++ code/connectivity_workflow/functions/get_set_parts.R
@@ -0,0 +1,33 @@
+get_set_parts <- function(set_num, api_key) {
+    url <- paste0(
+        "https://rebrickable.com/api/v3/lego/sets/",
+        set_num,
+        "/parts/?page=1&page_size=1000"
+    )
+    
+    resp <- GET(
+        url,
+        add_headers(Authorization = paste("key", api_key))
+    )
+    stop_for_status(resp)
+    
+    dat <- content(resp, as = "text", encoding = "UTF-8") |>
+        fromJSON()
+    
+    parts <- dat$results %>%
+        unnest_wider(part,  names_sep = "_") %>%
+        unnest_wider(color, names_sep = "_") %>%
+        dplyr::select(
+            part_num   = part_part_num,
+            part_name  = part_name,
+            color_id   = color_id,
+            color_name = color_name,
+            img_url    = part_part_img_url,
+            quantity
+        )
+   # if (!is.null(color_id_filter)) {
+   #     parts <- parts %>% filter(color_id %in% color_id_filter)
+   #}
+    
+    parts
+}
