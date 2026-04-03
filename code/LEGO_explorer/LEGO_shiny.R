cc5cc3f253f7a7f5cea5a84d8139f80310fa3140
pedroj <pedroj@me.com>
2026-04-03 20:18:12 +0200

    Updated shiny.R.

--- code/LEGO_explorer/LEGO_shiny.R
+++ code/LEGO_explorer/LEGO_shiny.R
@@ -21,36 +21,16 @@ library(here)
 
 
 # Fresh load EVERY time
-sets <- read_csv(here::here("data/rebrickable/sets.csv"))
-print("Original columns:"); print(colnames(sets))  # set_num, name, year, theme_id, num_parts
-
-# Rename year FIRST (year exists)
-sets <- sets %>%
-    dplyr::rename(year_numeric = year) %>%  # year -> year_numeric
-    dplyr::rename(set_theme_id = theme_id) %>%
-    dplyr::filter(!is.na(year_numeric))
-
-print("After rename:"); print(colnames(sets))  # Now has year_numeric
-print(class(sets$year_numeric))  # Should be "numeric"
-
-# Themes join
-themes <- read_csv(here::here("data/rebrickable/themes.csv"))
-themes <- themes %>% dplyr::select(id, theme_name = name) %>% dplyr::filter(!is.na(theme_name))
-sets <- sets %>% dplyr::left_join(themes, by = c("set_theme_id" = "id")) 
-
-print("Final columns:"); print(colnames(sets))  # Has year_numeric, theme_name
-
-print(nrow(sets))  # ~26k sets with themes
-print(sort(table(sets$theme_name), decreasing = TRUE)[1:5])  # Top: Star Wars, Town, etc.
-
+sets <- lego_sets
 ui <- fluidPage(
-    titlePanel("LEGO Sets Explorer"),
+    titlePanel("LEGO Explorer for individual parts"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("theme", "Theme:", choices = c("All", unique(sets$theme_name))),
+            selectInput("part_num", "Part number:", choices = c("All", unique(sets$part_num))),
             # Filter: filter(theme_name %in% input$theme)
                                     selected = c("Star Wars", "Ideas"), multiple = TRUE),
-            sliderInput("year", "Year:", min = 1950, max = 2025, value = c(2010, 2025), sep = "")
+            sliderInput("year", "Year:", min = 1950, max = 2025, 
+                        value = c(2010, 2025), sep = "")
         ),
         mainPanel(DTOutput("table"))
     )
@@ -82,7 +62,7 @@ server <- function(input, output) {
             # dplyr::mutate(image = paste0("<img src='https://images.rebrickable.com/sets/", 
             #                       set_num, "/", year_numeric, "/", 
             #                       set_num, ".jpg' height='100'></img>")) %>%
-            dplyr::select(any_of(c("image", "name", "set_num")), everything())
+            dplyr::select(any_of(c("image", "part_name", "set_num")), everything())
     })
     
     output$table <- renderDT({
